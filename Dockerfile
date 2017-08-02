3740ee947a62453a1d749734b14f3ce5723878c2
Jay Phillips <jicksta@gmail.com>
2017-08-02 11:17:56 -0400

    simplification

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
 FROM debian
 
-#ENV DEMETER_ENV $DEMETER_ENV
-ENV INSTALL_DIR '/app'
-
-RUN mkdir $INSTALL_DIR
-WORKDIR $INSTALL_DIR
-RUN mkdir mounted
+WORKDIR /app
 
 COPY ./watch-events-in-mounted /usr/bin/watch-events-in-mounted
 CMD watch-events-in-mounted
--- repro.sh
+++ repro.sh
@@ -4,7 +4,7 @@
 eval $(minikube docker-env)
 
 # build image and mount the `mounted` directory
-docker build -t repro:latest .
+docker build --tag repro .
 
 # Run image in container and attach
 docker run --rm --name repro-fs-reloading -v "$PWD/mounted":"/app/mounted" repro
